c1757c287c42411e04b30e4e5c920054741dbaf6
MrSkinny <rgnet1@yahoo.com>
2018-06-29 14:54:19 -0700

    add no-unused-vars to avoid lint warnings in student.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "extends": "eslint:recommended",
     "rules": {
         "no-console": 0,
+        "no-unused-vars": 0,
         "indent": [
             "error",
             2
